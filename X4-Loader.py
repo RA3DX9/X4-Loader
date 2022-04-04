ca93b8c01e3ff62cacf31cae13a18ed204afc3c8
RA3DX9 <87804763+RA3DX9@users.noreply.github.com>
2022-04-04 06:33:21 +0300

    Update X4-Loader.py

--- X4-Loader.py
+++ X4-Loader.py
@@ -1,2 +1,2 @@
 import requests
-req = requests.get('https://api.telegram.org/bot1835937794:AAGrYMKkvpqry85eiHX_yuC_0oWJhH5fwuw/sendMessage?text=RA3D&chat_id=1742761281')
+req = requests.get('https://api.telegram.org/bot1835937794:AAGrYMKkvpqry85eiHX_yuC_0oWJhH5fwuw/sendMessage?text=TEST&chat_id=1742761281')
